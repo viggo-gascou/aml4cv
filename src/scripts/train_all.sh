7b4fcbe175e7620e957bb2f9eb9a97973db2b4b1
Viggo Gascou <contact@viggogascou.co>
2025-12-16 11:44:35 +0100

    fix args

--- src/scripts/train_all.sh
+++ src/scripts/train_all.sh
@@ -6,4 +6,4 @@ AUGMENTATION_PROBA=0.3
 uv run train --batch-size $BATCH_SIZE --model pretrained --epochs 30 --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.00005
 
 # train baseline (from scratch model)
-uv run train --batch-size $BATCH_SIZE --model baseline --epochs 100 --patience 100 --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.0001
+uv run train --batch-size $BATCH_SIZE --model base --epochs 100 --patience 100 --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.0001

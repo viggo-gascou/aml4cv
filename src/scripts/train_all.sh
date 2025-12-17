c0c4766a1b7f051f27be89ce27c7f8b2591db640
Viggo Gascou <contact@viggogascou.co>
2025-12-17 20:29:50 +0100

    feat: update train script

--- src/scripts/train_all.sh
+++ src/scripts/train_all.sh
@@ -2,8 +2,10 @@
 
 BATCH_SIZE=64
 AUGMENTATION_PROBA=0.3
+EPOCHS=100
 # fine-tune pre-trained
-uv run train --batch-size $BATCH_SIZE --model pretrained --epochs 30 --patience 30 --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.00005
+uv run train --batch-size $BATCH_SIZE --model pretrained --epochs $EPOCHS --patience $EPOCHS --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.00005 --early-stop-criterion val/loss
+
 
 # train baseline (from scratch model)
-uv run train --batch-size $BATCH_SIZE --model base --epochs 100 --patience 100 --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.0001
+uv run train --batch-size $BATCH_SIZE --model base --epochs $EPOCHS --patience $EPOCHS --augmentation-proba $AUGMENTATION_PROBA --learning-rate 0.0001 --early-stop-criterion val/loss
